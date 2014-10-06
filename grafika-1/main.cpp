d0e1a2001d4a08fcb7ee4dff5a93be1164effcfe
coldcue <coldcue@gmail.com>
2014-10-06 22:43:59 +0200

    Initial Commit

---
+++ grafika-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grafika-1
+//
+//  Created by Andras Szell on 10/6/14.
+//  Copyright (c) 2014 Andras Szell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
